02cae1af246f86a3c0180fa0298aee7eb68763be
sunkyung95 <37947951+sunkyung95@users.noreply.github.com>
2020-11-20 11:31:44 +0900

    2020.11.20_admin_신고내역 조회기능 수정 및 추가
    - mybatis-config.xml : 신고내역 조회기능관련 내용 추가 밎 수정
    - 신고내역 조회기능 관련 class, xml, css 수정 및추가

--- HOTSPOT/src/main/java/com/kh/hotspot/admin/model/dao/AdminReportDao.java
+++ HOTSPOT/src/main/java/com/kh/hotspot/admin/model/dao/AdminReportDao.java
@@ -1,8 +1,30 @@
 package com.kh.hotspot.admin.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.kh.hotspot.admin.model.vo.Report;
+import com.kh.hotspot.common.model.vo.PageInfo;
 
 @Repository
 public class AdminReportDao {
+	
+	public int selectListCount(SqlSessionTemplate sqlSession) {
+		return sqlSession.selectOne("reportMapper.selectListCount");
+	}
+	
+	public ArrayList<Report> selectList(SqlSessionTemplate sqlSession, PageInfo pageInfo) {
+		
+		int offset = (pageInfo.getCurrentPage() - 1) * pageInfo.getBoardLimit();
+		int limit = pageInfo.getBoardLimit();
+		
+		RowBounds rowBounds = new RowBounds(offset, limit);
+		
+		return (ArrayList)sqlSession.selectList("reportMapper.selectList", null, rowBounds);
+		
+	}
 
 }
