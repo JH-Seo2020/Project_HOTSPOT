018c353b7f0140dd16fa98d93b8432862f5fc35d
JH-Seo2020 <71337347+JH-Seo2020@users.noreply.github.com>
2020-11-20 19:52:15 +0900

    1120_메인용Controller_서지혜

--- HOTSPOT/src/main/java/com/kh/hotspot/guest/space/controller/SpaceController.java
+++ HOTSPOT/src/main/java/com/kh/hotspot/guest/space/controller/SpaceController.java
@@ -1,5 +1,14 @@
 package com.kh.hotspot.guest.space.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SpaceController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "main";
+	}
 
 }
