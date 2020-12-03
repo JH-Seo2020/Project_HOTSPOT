0ce0c8601f177161dc28fa144bd1f0d132d55442
nowwoo94 <nowwoo11@gmail.com>
2020-12-03 10:21:45 +0900

    chatbot

--- HOTSPOT/src/main/java/com/kh/hotspot/admin/chat/model/dao/ChatBotDao.java
+++ HOTSPOT/src/main/java/com/kh/hotspot/admin/chat/model/dao/ChatBotDao.java
@@ -1,8 +1,22 @@
 package com.kh.hotspot.admin.chat.model.dao;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ChatBotDao {
+import com.kh.hotspot.admin.chat.model.vo.ChatBot;
 
+@Repository
+public class ChatBotDao {
+
+	public ArrayList<ChatBot> selectList(SqlSessionTemplate sqlSession){
+		
+		return (ArrayList)sqlSession.selectList("chatbotMapper.selectList");
+	}
+	
+	public ArrayList<ChatBot> chatList(SqlSessionTemplate sqlSession, String message){
+		
+		return (ArrayList)sqlSession.selectList("chatbotMapper.chatList", message);
+	}
 }
